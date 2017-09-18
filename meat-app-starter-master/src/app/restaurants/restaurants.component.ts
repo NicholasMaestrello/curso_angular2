9b49e6349af6271b8bc70e615f142e64fa4222ef
NicholasMaestrello <rainickmr@gmail.com>
2017-09-17 21:07:33 -0300

    funcionando exercicio restaurante

--- meat-app-starter-master/src/app/restaurants/restaurants.component.ts
+++ meat-app-starter-master/src/app/restaurants/restaurants.component.ts
@@ -1,35 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 import {Restaurant} from './restaurant/restaurant.model'
+import {RestaurantsService} from './restaurants.service'
 
 @Component({
   selector: 'mt-reataurants',
   templateUrl: './restaurants.component.html',
 })
 export class RestaurantsComponent implements OnInit {
+  restaurants: Restaurant[]
 
-  restaurants: Restaurant[] = [
-    {
-      id: "bread-bakery",
-      name: "Bread & Bakery",
-      category: "Bakery",
-      deliveryEstimate: "25m",
-      rating: 4.9,
-      imagePath: "assets/img/restaurants/breadbakery.png"
-    },
-    {
-      id: "burger-house",
-      name: "Burger House",
-      category: "Hamburgers",
-      deliveryEstimate: "100m",
-      rating: 3.5,
-      imagePath: "assets/img/restaurants/burgerhouse.png"
-    },
-  ]
-
-  constructor() { }
+  constructor(private restaurantsService: RestaurantsService) { }
 
   ngOnInit() {
+    this.restaurantsService.restaurants()
+    .subscribe(restaurants => this.restaurants = restaurants)
   }
 
 }
