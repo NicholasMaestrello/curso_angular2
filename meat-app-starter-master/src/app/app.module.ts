60195f529a2b7a671758fa1306d41aa4afcbca0d
NicholasMaestrello <rainickmr@gmail.com>
2017-09-24 20:39:59 -0300

    implemetnando modulos

--- meat-app-starter-master/src/app/app.module.ts
+++ meat-app-starter-master/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 import { ROUTES } from './app.routes'
 
@@ -10,7 +10,6 @@ import { ROUTES } from './app.routes'
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component'
 import { RestaurantsService } from './restaurants/restaurants.service';
@@ -20,22 +19,16 @@ import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shoppin
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
-import { OrderComponent } from './order/order.component';
-import { InputComponent } from './shared/input/input.component';
-import { RadioComponent } from './shared/radio/radio.component';
-import { OrderItemsComponent } from './order/order-items/order-items.component'
 import { OrderService } from './order/order.service';
-import { DeliveryCostsComponent } from './delivery-costs/delivery-costs.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
-import { RatingComponent } from './/shared/rating/rating.component'
 
+import {SharedModule} from './shared/shared.module'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    AboutComponent,
     RestaurantsComponent,
     RestaurantComponent,
     RestaurantDetailComponent,
@@ -43,18 +36,14 @@ import { RatingComponent } from './/shared/rating/rating.component'
     ShoppingCartComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderComponent,
-    InputComponent,
-    RadioComponent,
-    OrderItemsComponent,
-    DeliveryCostsComponent,
-    OrderSummaryComponent,
-    RatingComponent
+    OrderSummaryComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     FormsModule,
+    ReactiveFormsModule,
+    SharedModule,
     RouterModule.forRoot(ROUTES)
   ],
   providers: [RestaurantsService, ShoppingCartService, OrderService, { provide: LOCALE_ID, useValue: 'pt-BR' }],
--- meat-app-starter-master/src/app/shared/input/input.component.ts
+++ meat-app-starter-master/src/app/shared/input/input.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, ContentChild, AfterContentInit } from '@angular/core';
-import {NgModel} from '@angular/forms';
+import {NgModel, FormControlName} from '@angular/forms';
 
 
 @Component({
@@ -13,15 +13,17 @@ export class InputComponent implements OnInit, AfterContentInit {
   @Input() errorMessage:string;
 
   @ContentChild(NgModel) model: NgModel;
+  @ContentChild(FormControlName) control: FormControlName
+
   constructor() { }
 
   ngOnInit() {
   }
 
   ngAfterContentInit(){
-    this.input = this.model
+    this.input = this.model || this.control
     if(this.input === undefined){
-      throw new Error('Esse componente precisa ser usado com uma diretiva')
+      throw new Error('Esse componente precisa ser usado com uma diretiva ng model ou form control name')
     }
   }
 
